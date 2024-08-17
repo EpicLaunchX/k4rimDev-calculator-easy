44c7ece3b3fa47895e91acdce614c5f78311cc88
Karim Mirzaguliyev <kenanovkenan299@gmail.com>
2024-08-17 11:44:02 +0400

    test: task 2

--- tests/test_operands.py
+++ tests/test_operands.py
@@ -1,7 +1,11 @@
-from src.pytemplate.domain.models import Operands
+from src.pytemplate.domain.models import Operands, operands_factory
 
 
 def test_operands_type():
     operands = Operands(2, 4)
     assert operands.first_operand == 2
     assert operands.second_operand == 4
+
+
+def test_operands_factory_type():
+    assert isinstance(operands_factory(1, 4), Operands)
